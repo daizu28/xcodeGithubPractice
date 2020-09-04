361910938af355c8b16ada1bf1c12caac02f88b7
Reina Kobayashi <kinokodaizukinako@gmail.com>
2020-09-04 13:10:17 +0900

    Initial Commit

---
+++ aaa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  aaa
+//
+//  Created by 小林玲衣奈 on 2020/09/04.
+//  Copyright © 2020 com.litech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
